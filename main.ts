8faa5c6402a063029ff440112842a7fae602f4a1
Russell Blickhan <rwblickhan@gmail.com>
2023-01-20 13:59:52 -0800

    Rename plugin

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface TagSearchPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: TagSearchPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TagSearchPlugin extends Plugin {
+	settings: TagSearchPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: TagSearchPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TagSearchPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}

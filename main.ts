d478ff8a137c32cf15bf13839cdc593214fc9f22
Russell Blickhan <rwblickhan@gmail.com>
2023-01-20 16:25:09 -0800

    Fully implement

--- main.ts
+++ main.ts
@@ -1,126 +1,78 @@
-import {
-	App,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
-
-// Remember to rename these classes and interfaces!
-
-interface TagSearchPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: TagSearchPluginSettings = {
-	mySetting: "default",
-};
+import { App, Plugin, FuzzySuggestModal, getAllTags, Modal } from "obsidian";
 
 export default class TagSearchPlugin extends Plugin {
-	settings: TagSearchPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("Hello, you!");
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: "open-tag-search",
 			name: "Open tag search",
 			callback: () => {
-				new SampleModal(this.app).open();
+				/* eslint-disable @typescript-eslint/no-explicit-any */
+				const searchPlugin = (
+					this.app as any
+				).internalPlugins.getPluginById("global-search");
+				/* eslint-enable @typescript-eslint/no-explicit-any */
+				const search = searchPlugin && searchPlugin.instance;
+
+				if (searchPlugin && searchPlugin.instance) {
+					new TagSearchModal(this.app, search).open();
+				} else {
+					new UnsupportedTagSearchModel(this.app).open();
+				}
 			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {}
+}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
+interface Search {
+	openGlobalSearch(_: string): void;
+}
+
+class TagSearchModal extends FuzzySuggestModal<string> {
+	search: Search;
+
+	constructor(app: App, search: Search) {
+		super(app);
+		this.search = search;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	getItems(): string[] {
+		const files = app.vault.getAllLoadedFiles();
+		const itemSet = new Set<string>();
+		for (const file of files) {
+			const cache = app.metadataCache.getCache(file.path);
+			if (cache === null) {
+				continue;
+			}
+			getAllTags(cache)?.forEach((tag) => {
+				itemSet.add(tag);
+			});
+		}
+		return Array.from(itemSet);
+	}
+
+	getItemText(item: string): string {
+		return item;
+	}
+
+	onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
+		this.search.openGlobalSearch(`tag:${item}`);
 	}
 }
 
-class SampleModal extends Modal {
+class UnsupportedTagSearchModel extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText("Woah!");
+		contentEl.setText("Please enable the search core plugin!");
 	}
 
 	onClose() {
 		const { contentEl } = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: TagSearchPlugin;
-
-	constructor(app: App, plugin: TagSearchPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
 	}
 }
